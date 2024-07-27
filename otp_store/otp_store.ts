e9190240f4d093d1b9875846d37343e87b2a17e9
Manan Gandhi <ardumanan@gmail.com>
2024-07-27 20:51:35 +0530

    Completed OTPStore abstract class with implementation of RedisStore

---
+++ otp_store/otp_store.ts
@@ -0,0 +1,7 @@
+abstract class OTPStore {
+    abstract get(key: string): Promise<number>;
+    abstract set(key: string, value: number, ttl?: number): Promise<string | null>;
+    abstract del(key: string): Promise<number | null>;
+}
+
+export default OTPStore;
---
+++ otp_store/redis_store.ts
@@ -0,0 +1,28 @@
+import OTPStore from './otp_store'
+import { RedisClientType, RedisFunctions, RedisModules, RedisScripts } from '@redis/client';
+
+type RedisClient = RedisClientType<RedisFunctions, RedisModules, RedisScripts>
+
+class RedisStore extends OTPStore {
+    private client: RedisClient;
+
+    constructor(client: RedisClient) {
+        super()
+        this.client = client
+    }
+
+    async get(key: string): Promise<number> {
+        const value = await this.client.get(key)
+        return value ? parseInt(value) : 0
+    }
+
+    async set(key: string, value: number, ttl = 60): Promise<string | null> {
+        return await this.client.set(key, value.toString(), { EX: ttl })
+    }
+
+    async del(key: string): Promise<number | null> {
+        return await this.client.del(key)
+    }
+}
+
+export default RedisStore
