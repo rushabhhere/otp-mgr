9c308dd172035832e357311b287c119890c7784e
Manan Gandhi <ardumanan@gmail.com>
2024-07-27 15:59:32 +0530

    Move to classes

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
-const otpMap = new Map();
 
 type Generate = (userId: string, purpose: number) => number;
 type Verify = (userId: string, purpose: number, enteredOtp: number) => boolean;
 
-const generate: Generate = (userId, purpose) => {
-  const otp = Math.floor(1000 + Math.random() * 9000);
-  otpMap.set(`${userId}:${purpose}`, otp);
-  return otp;
-};
+class OtpMgr {
+  otpMap: Map<string, number> = new Map();
 
-const verify: Verify = (userId, purpose, enteredOtp) => {
-  const otp = otpMap.get(`${userId}:${purpose}`);
-  return otp === enteredOtp;
-};
+  generate: Generate = (userId, purpose) => {
+    const otp: number = Math.floor(1000 + Math.random() * 9000);
+    this.otpMap.set(`${userId}:${purpose}`, otp);
+    return otp;
+  };
 
-export { generate, verify };
+  verify: Verify = (userId, purpose, enteredOtp) => {
+    const otp = this.otpMap.get(`${userId}:${purpose}`);
+    return otp === enteredOtp;
+  };
+}
+
+module.exports = OtpMgr;
