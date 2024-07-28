4992345860c75116c91e600eb646d59cf530917a
Manan Gandhi <ardumanan@gmail.com>
2024-07-28 16:42:16 +0530

    Prettier Formatting

--- otp_store/memcached_store.ts
+++ otp_store/memcached_store.ts
@@ -4,50 +4,50 @@ import Memcached from 'memcached';
 type MemcachedClient = Memcached;
 
 class MemcachedStore extends OTPStore {
-  private client: MemcachedClient;
+    private client: MemcachedClient;
 
-  constructor(client: MemcachedClient) {
-    super();
-    this.client = client;
-  }
+    constructor(client: MemcachedClient) {
+        super();
+        this.client = client;
+    }
 
-  async get(key: string): Promise<number> {
-    var value = 0;
-    await new Promise<void>((resolve, reject) => {
-      this.client.get(key, (err, data) => {
-        if (err) {
-          reject();
-        }
-        value = data;
-        resolve();
-      });
-    });
-    return value ?? 0;
-  }
+    async get(key: string): Promise<number> {
+        var value = 0;
+        await new Promise<void>((resolve, reject) => {
+            this.client.get(key, (err, data) => {
+                if (err) {
+                    reject();
+                }
+                value = data;
+                resolve();
+            });
+        });
+        return value ?? 0;
+    }
 
-  async set(key: string, value: number, ttl = 60): Promise<string | null> {
-    await new Promise<void>((resolve, reject) => {
-      this.client.set(key, value, ttl, err => {
-        if (err) {
-          reject();
-        }
-        resolve();
-      });
-    });
-    return null;
-  }
+    async set(key: string, value: number, ttl = 60): Promise<string | null> {
+        await new Promise<void>((resolve, reject) => {
+            this.client.set(key, value, ttl, err => {
+                if (err) {
+                    reject();
+                }
+                resolve();
+            });
+        });
+        return null;
+    }
 
-  async del(key: string): Promise<number | null> {
-    await new Promise<void>((resolve, reject) => {
-      this.client.del(key, err => {
-        if (err) {
-          reject();
-        }
-        resolve();
-      });
-    });
-    return null;
-  }
+    async del(key: string): Promise<number | null> {
+        await new Promise<void>((resolve, reject) => {
+            this.client.del(key, err => {
+                if (err) {
+                    reject();
+                }
+                resolve();
+            });
+        });
+        return null;
+    }
 }
 
 export default MemcachedStore;
--- tests/otp_store/map_store.test.ts
+++ tests/otp_store/map_store.test.ts
@@ -3,14 +3,14 @@ import RedisStore from '../../otp_store/redis_store';
 import MapStore from '../../otp_store/map_store';
 
 test('Test Map Store', async () => {
-  const mapStore: MapStore = new MapStore();
+    const mapStore: MapStore = new MapStore();
 
-  await mapStore.set('test', 123, 60).then(() => {});
-  await mapStore.get('test').then(value => {
-    expect(value).toBe(123);
-  });
-  await mapStore.del('test').then(() => {});
-  await mapStore.get('test').then(value => {
-    expect(value).toBe(0);
-  });
+    await mapStore.set('test', 123, 60).then(() => { });
+    await mapStore.get('test').then(value => {
+        expect(value).toBe(123);
+    });
+    await mapStore.del('test').then(() => { });
+    await mapStore.get('test').then(value => {
+        expect(value).toBe(0);
+    });
 });
